b1235c72c5a359b45ba1d43c190e547790c6cbf9
Tim Lee <Tim8_Lee@pegatroncorp.com>
2024-01-17 15:21:29 +0800

    modify gdb.c

--- week1/c pointer/gdb.c
+++ week1/c pointer/gdb.c
@@ -1,4 +1,13 @@
+#include<stdio.h>
 int a[3];
 struct { double v[3]; double length; } b[17];
 int calendar[12][31];
-int main() { }
+int main() {
+
+    b[0].v[0] = 1;
+    b[0].v[1] = 2;
+    b[0].v[2] = 3;
+
+    printf("Set breakpoint here.\n");
+
+}
