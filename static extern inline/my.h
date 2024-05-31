01c3c431191fa6e35f2d826aeade3009f611d620
Tim Lee <Tim8_Lee@pegatroncorp.com>
2024-05-31 09:53:31 +0800

    message

--- static extern inline/my.h
+++ static extern inline/my.h
@@ -1,9 +1,13 @@
+#ifdef IS_STATIC_INLINE
 #if IS_STATIC_INLINE == 'y'
 #include "function.h"
 static inline void func()
 {
    function();
 }
+#else
+extern void func();
+#endif
 #else
 extern void func();
 #endif
