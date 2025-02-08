85ccef78c1129ded25521e1740807810fb74b4f5
OuO333333 <37506309+OuO333333@users.noreply.github.com>
2025-02-08 22:34:40 +0800

    Update middle_of_linklist.cpp

--- C/linklist/middle_of_linklist.cpp
+++ C/linklist/middle_of_linklist.cpp
@@ -1,12 +1,23 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
 
 // 定義單鏈表結構
-struct ListNode {
+typedef struct ListNode {
     int val;
-    ListNode* next;
-    ListNode(int x) : val(x), next(NULL) {}  // 構造函數
-};
+    struct ListNode* next;
+} ListNode;
+
+// 創建新節點
+ListNode* createNode(int x) {
+    ListNode* newNode = (ListNode*)malloc(sizeof(ListNode));
+    if (!newNode) {
+        printf("內存分配失敗\n");
+        exit(1);
+    }
+    newNode->val = x;
+    newNode->next = NULL;
+    return newNode;
+}
 
 // 找到中間節點的函數
 ListNode* findMiddle(ListNode* head) {
@@ -24,20 +35,32 @@ ListNode* findMiddle(ListNode* head) {
     return slow;  // 返回中間節點
 }
 
+// 釋放鏈表內存
+void freeList(ListNode* head) {
+    ListNode* temp;
+    while (head) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
 // 找到鏈結串列的中間節點（Find the Middle Node of a Linked List）
 int main() {
     // 創建單鏈表 1 -> 2 -> 3 -> 4 -> 5
-    ListNode* head = new ListNode(1);
-    head->next = new ListNode(2);
-    head->next->next = new ListNode(3);
-    head->next->next->next = new ListNode(4);
-    head->next->next->next->next = new ListNode(5);
+    ListNode* head = createNode(1);
+    head->next = createNode(2);
+    head->next->next = createNode(3);
+    head->next->next->next = createNode(4);
+    head->next->next->next->next = createNode(5);
 
     ListNode* mid = findMiddle(head);
     if (mid) {
-        cout << "中間節點值：" << mid->val << endl;  // 輸出 3
+        printf("中間節點值：%d\n", mid->val);  // 輸出 3
     }
+
+    // 釋放內存
+    freeList(head);
 
     return 0;
 }
-
